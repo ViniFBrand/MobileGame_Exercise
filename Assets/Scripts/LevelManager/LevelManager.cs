829f0526e6d10f4f61a4d110ca762ba5e3665b10
Vini <vfbrandx6@gmail.com>
2025-07-22 22:42:34 -0300

    Added Coroutine to LevelManager to animate spawned pieces by scaling
    Adicionou Corotina "ScalePieceByTime" no LevelManager para auxiliar na animação de criação de peças criadas

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class LevelManager : MonoBehaviour
 {
@@ -18,7 +19,10 @@ public class LevelManager : MonoBehaviour
     private List<LevelPieceBase> _spawnedPieces = new List<LevelPieceBase>();
     [SerializeField] private LevelPieceBasedSetup _currSetup;
 
-
+    [Header("Animation")]
+    public float scaleDuration = .2f;
+    public float scaleTimeBetweenPieces = .1f;
+    public Ease ease = Ease.OutBack;
 
 
     private void Awake()
@@ -84,9 +88,25 @@ public class LevelManager : MonoBehaviour
 
         ColorManager.Instance.ChangeColorByType(_currSetup.ArtType);
 
+        StartCoroutine(ScalePieceByTime());
+
     }
 
-    
+    IEnumerator ScalePieceByTime()
+    {
+        foreach(var p in _spawnedPieces)
+        {
+            p.transform.localScale = Vector3.zero;
+        }
+
+        yield return null;
+
+        for(int i = 0; i < _spawnedPieces.Count; i++)
+        {
+            _spawnedPieces[i].transform.DOScale(1, scaleDuration).SetEase(ease);
+            yield return new WaitForSeconds(scaleTimeBetweenPieces);
+        }
+    }
 
     private void CreateLevelPiece(List<LevelPieceBase> list)
     {
